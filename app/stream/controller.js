fce6eebdbf7ce9b2ef1e91c323669bb2c74527ce
Graeme Longman <graemelongman@gmail.com>
2016-08-21 15:01:15 +0200

    Allows comments to be created over WS

--- app/stream/controller.js
+++ app/stream/controller.js
@@ -4,47 +4,28 @@ const {
 } = Ember;
 
 export default Ember.Controller.extend({
+
   cable: Ember.inject.service(),
-
-  setupConsumer: Ember.on('init', function() {
-
-    var consumer = this.get('cable').createConsumer('ws://localhost:3000/cable');
-
-    // consumer.subscriptions.create("CommentsChannel", {
-    //   connected() {
-    //     debug('connected');
-    //     // this.perform('hello', {
-    //     //   foo: 'bar'
-    //     // });
-    //     // this.perform('hello');
-    //   },
-    //   received(data) {
-    //     debug("received(data) -> " + Ember.inspect(data));
-    //     this.addComment(data);
-    //   },
-    //   disconnected() {
-    //     debug("CommentsChannel#disconnected");
-    //   }
-    // });
-
-    // Passing Parameters to Channel
-    const subscription = consumer.subscriptions.create({
-      channel: 'CommentsChannel'
-    }, {
-      received: (data) => {
-        this.addComment(data.comment);
-      }
-    });
-
-    // Send actions to your Action Cable channel class
-    // subscription.perform("your_channel_action", {
-    //   hey: "hello"
-    // });
-  }),
 
   comments: [],
 
-  addComment(comment) {
-    this.get('comments').pushObject(comment);
+  setupSubscription: Ember.on('init', function() {
+    var consumer = this.get('cable').createConsumer('ws://localhost:3000/cable');
+
+    var subscription = consumer.subscriptions.create("CommentsChannel", {
+      received: (data) => {
+        this.get('comments').pushObject(data.comment);
+      }
+    });
+
+    this.set('subscription', subscription);
+  }),
+
+  actions: {
+    doComment() {
+      this.get('subscription').send({
+        body: this.get('body')
+      });
+    }
   }
 })
