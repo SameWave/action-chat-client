7adf28bb9d3fbbc8b5c8bc24ce5b3b244cd05c66
Monde <monde@samewave.com>
2016-09-09 13:35:21 +0200

    Create auto resizable textarea component

---
+++ app/components/auto-resize-textarea/component.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+const {
+  TextArea,
+  run
+} = Ember;
+
+export default TextArea.extend({
+  _resize() {
+    let textArea = this.element;
+
+    run.later(this, function() {
+      textArea.style.cssText = 'height:auto; padding:0';
+      textArea.style.cssText = `height:${textArea.scrollHeight}px`;
+    }, 0);
+  },
+
+  keyDown() {
+    this._resize();
+  }
+});
