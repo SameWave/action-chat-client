abebfbe39f390730390f2e42bb30cc41f355e16d
Monde <monde@samewave.com>
2016-09-12 11:09:59 +0200

    Move stream chat model back to stream model

---
+++ app/stream/model.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+const {
+  Model,
+  attr,
+  hasMany
+} = DS;
+
+export default Model.extend({
+  title: attr('string'),
+
+  comments: hasMany('comment', {
+    inverse: 'stream',
+    async: true
+  })
+});
