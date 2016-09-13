dbfcbee450a685de6bf6917e0b786dd1606c8b74
Graeme Longman <graemelongman@gmail.com>
2016-09-13 12:19:32 +0200

    Moves header into wormhole to prevent session / transition issues

--- app/application/route.js
+++ app/application/route.js
@@ -8,9 +8,14 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   },
 
   sessionAuthenticated() {
+    console.log('authenticationSucceeded');
     this._loadSessionPerson().then(() => {
       this.transitionTo('streams');
     }).catch(() => this.get('session').send('logout'));
+  },
+
+  invalidationSucceeded() {
+    console.log('invalidationSucceeded');
   },
 
   _loadSessionPerson() {
