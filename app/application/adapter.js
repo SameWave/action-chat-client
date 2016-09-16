849da283f9245327626166a5a73cb4b2c8626a89
Monde <monde@samewave.com>
2016-09-16 10:18:51 +0200

    WIP

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -3,11 +3,15 @@ import Ember from 'ember';
 import ENV from 'action-chat-client/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+const {
+	JSONAPIAdapter
+} = DS;
+
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.host,
   authorizer: 'authorizer:custom',
 
-  pathForType: function(type) {
+  pathForType(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
   }
 
