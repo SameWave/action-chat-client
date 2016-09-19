4165dbbd24891e86f17cffe9f77f086a31ae3181
Monde <monde@samewave.com>
2016-09-19 11:00:59 +0200

    Add assertion to friendly-date test

--- tests/integration/components/friendly-date/component-test.js
+++ tests/integration/components/friendly-date/component-test.js
@@ -1,24 +1,14 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('friendly-date', 'Integration | Component | friendly date', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('testDate', moment('2016-11-19 08:30'));
+  this.render(hbs`{{friendly-date date=testDate isLocal=true}}`);
 
-  this.render(hbs`{{friendly-date}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#friendly-date}}
-      template block text
-    {{/friendly-date}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '08:30');
 });
