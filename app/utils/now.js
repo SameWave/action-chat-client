33db95a765cd4fe9f1f200b1766354a73125bc5c
Monde <monde@samewave.com>
2016-09-19 10:47:42 +0200

    Create logic for now utility

--- app/utils/now.js
+++ app/utils/now.js
@@ -1,3 +1,14 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+const {
+  testing
+} = Ember;
+
+// isFrozen = true; // Useful for developing entries related features or debugging tests
 export default function now() {
-  return true;
+  let isFrozen = testing;
+  let timestamp = '2014-12-09T07:59:59+00:00';
+
+  return isFrozen ? moment.utc(timestamp).clone() : moment.utc().clone();
 }
