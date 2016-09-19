4090528665efa649cc2ac9e6acd8d1d23aa7e651
Monde <monde@samewave.com>
2016-09-19 10:45:51 +0200

    generate now utility

---
+++ tests/unit/utils/now-test.js
@@ -0,0 +1,10 @@
+import now from 'action-chat-client/utils/now';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | now');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = now();
+  assert.ok(result);
+});
