c99a8f76319eaaeb54917f4b5f0497ec5bd6a19b
Monde <monde@samewave.com>
2016-09-23 08:51:40 +0200

    Rename mention-list test

--- tests/integration/components/action-sheet/component-test.js
+++ tests/integration/components/action-sheet/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('action-sheet', 'Integration | Component | action sheet', {
+moduleForComponent('mention-list', 'Integration | Component | action sheet', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{action-sheet}}`);
+  this.render(hbs`{{mention-list}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#action-sheet}}
+    {{#mention-list}}
       template block text
-    {{/action-sheet}}
+    {{/mention-list}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
