0c47f3372d7f2375f05fe29e23ed0afc632e2804
Graeme Longman <graemelongman@gmail.com>
2016-09-30 06:46:18 +0200

    Hide ds.json-api-adapter.ajax-options deprecation warnings

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,7 @@
+window.deprecationWorkflow = window.deprecationWorkflow || {};
+window.deprecationWorkflow.config = {
+  workflow: [{
+    handler: "silence",
+    matchId: "ds.json-api-adapter.ajax-options"
+  }]
+};
