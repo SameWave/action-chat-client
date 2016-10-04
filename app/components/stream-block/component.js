c16e0af298cea4594e9f79b1b8997ed731cce98f
Monde <monde@samewave.com>
2016-10-04 15:57:45 +0200

    Add missing mixin to stream block

--- app/components/stream-block/component.js
+++ app/components/stream-block/component.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import SwipableListItemMixin from 'action-chat-client/mixins/swipable-list-item';
 
 const {
 	Component,
 	computed
 } = Ember;
 
-export default Component.extend({
+export default Component.extend(SwipableListItemMixin, {
   classNames: ['c-media-block--stream'],
 
   stream: null,
