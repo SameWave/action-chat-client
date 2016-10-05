c9d3ac6520c412e9c639e6334e65a62dac41aaf3
Graeme Longman <graemelongman@gmail.com>
2016-10-05 09:18:23 +0200

    Fixes typos

--- app/components/media-block/goal/component.js
+++ app/components/media-block/goal/component.js
@@ -5,7 +5,7 @@ export default MediaBlockComponent.extend({
   classNames: ['c-media-block--goal'],
 
   actions: {
-    deletGoal() {
+    deleteGoal() {
       if (this.get('onDeleteGoal')) {
         this.get('onDeleteGoal')(this);
       }
