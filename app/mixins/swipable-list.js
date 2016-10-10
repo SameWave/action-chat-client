24848d29b9d1ab235d39a1037af6397f5c8753b2
Monde <monde@samewave.com>
2016-10-10 11:23:02 +0200

    Re-enable scrolling after panning

--- app/mixins/swipable-list.js
+++ app/mixins/swipable-list.js
@@ -12,8 +12,8 @@ export default Mixin.create(RecognizerMixin, {
 
   scroll: service(),
 
-  // recognizers: 'tap pan',
-  recognizers: 'tap',
+  recognizers: 'tap pan',
+  // recognizers: 'tap',
 
   items: {},
   previousItem: null,
@@ -23,13 +23,6 @@ export default Mixin.create(RecognizerMixin, {
 
   didInsertElement() {
     this._super(...arguments);
-    this._enableScroll();
-  },
-
-  _enableScroll() {
-    if (this.$scrollContainer) {
-      this.get('scroll').enable(this.$scrollContainer, this.onScrollCallback);
-    }
   },
 
   _getItemFromEvent(event) {
@@ -43,49 +36,49 @@ export default Mixin.create(RecognizerMixin, {
     }
   },
 
-  // panStart(event) {
-  //   console.log(this.get('scroll.active'));
-  //   if (this.get('scroll.active')) {
-  //     return;
-  //   }
+  panStart(event) {
+    console.log(this.get('scroll.active'));
+    if (this.get('scroll.active')) {
+      return;
+    }
 
-  //   this.get('scroll').disable();
+    this.get('scroll').disable();
 
-  //   this.currentItem = this._getItemFromEvent(event);
+    this.currentItem = this._getItemFromEvent(event);
 
-  //   if (this.previousItem) {
-  //     this.previousItem.doSwipeRight(event);
-  //   }
+    if (this.previousItem) {
+      this.previousItem.doSwipeRight(event);
+    }
 
-  //   if (this.currentItem) {
-  //     this.currentItem.doPanStart(event);
-  //   }
-  // },
+    if (this.currentItem) {
+      this.currentItem.doPanStart(event);
+    }
+  },
 
-  // panMove(event) {
+  panMove(event) {
 
-  //   if (this.get('scroll.active')) {
-  //     return;
-  //   }
+    if (this.get('scroll.active')) {
+      return;
+    }
 
-  //   if (this.currentItem) {
-  //     this.currentItem.doPanMove(event);
-  //   }
-  // },
+    if (this.currentItem) {
+      this.currentItem.doPanMove(event);
+    }
+  },
 
-  // panEnd(event) {
-  //   if (this.get('scroll.active')) {
-  //     return;
-  //   }
+  panEnd(event) {
+    if (this.get('scroll.active')) {
+      return;
+    }
 
-  //   this.previousItem = this.currentItem;
+    this.previousItem = this.currentItem;
 
-  //   if (this.currentItem) {
-  //     this.currentItem.doPanEnd(event);
-  //     // this.currentItem = null;
-  //   }
+    if (this.currentItem) {
+      this.currentItem.doPanEnd(event);
+      // this.currentItem = null;
+    }
 
-  //   this._enableScroll();
-  // }
+    this.get('scroll').enable();
+  }
 
 });
